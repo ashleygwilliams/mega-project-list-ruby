95cdb00d6526e0d5109dd57ec8fd334944c9ac9d
ashley williams <heyashleyashley@gmail.com>
2013-07-13 18:16:53 -0400

    add solution for 1-1

---
+++ 1 - Numbers/1-1  Find PI to the Nth Digit/1-1_solution.rb
@@ -0,0 +1,8 @@
+print "Please enter the number of decimal places you would like: "
+user_places = gets
+
+def findPI(decimal_places)
+	Math::PI.round(decimal_places)
+end
+
+puts findPI(user_places.to_i)
